e1b935dad80f8a480fb3a4f5da73207ff1c08a0f
Jeff <42256814+jeff-gosselin@users.noreply.github.com>
2020-04-03 04:27:05 -0400

    initial setup
    My first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { gameMode } from "./functions.js";
 
 export default function App() {
+  const [playerMode, setPlayerMode] = useState(null);
+
+  function gameMode(e, players) {
+    players === 1 ? setPlayerMode(1) : setPlayerMode(2);
+  }
+
+  console.log(playerMode);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 onClick={e => gameMode(e, 1)}>Bye CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
