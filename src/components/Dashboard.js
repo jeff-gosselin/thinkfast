67f28edb52f333ab07a86763c98a5a0bcc89723f
Jeff <42256814+jeff-gosselin@users.noreply.github.com>
2020-04-09 17:15:38 -0400

    Working on clock

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,6 +6,7 @@ export default function Dashboard() {
   return (
     <div id="dashboard">
       <LogoText className="header-logo" />
+
       <div className="scoreboard">
         <div className="scoreboard-p1">
           <h2>PLAYER 1</h2>
@@ -13,6 +14,7 @@ export default function Dashboard() {
             0<span>pts</span>
           </h3>
         </div>
+
         <div className="scoreboard-p2">
           <h2>PLAYER 2</h2>
           <h3 className="score">
@@ -20,6 +22,14 @@ export default function Dashboard() {
           </h3>
         </div>
       </div>
+
+      <div className="turn-indicators">
+        <div className="turn-indicators-p1" />
+        <div className="turn-indicators-p2 inactive" />
+      </div>
+
+      <div className="clock start">START</div>
+      <div className="clock">00:00</div>
     </div>
   );
 }
