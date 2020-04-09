8d905c0de129d205462e0eabe45050c67ab2f7ae
Jeff <42256814+jeff-gosselin@users.noreply.github.com>
2020-04-09 13:22:27 -0400

    Working on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { ReactComponent as LogoText } from "../svgs/logo-text.svg";
 import "../styles/Dashboard.scss";
 
 export default function Dashboard() {
   return (
     <div id="dashboard">
-      <h1 />
+      <LogoText className="header-logo" />
+      <div className="scoreboard">
+        <div className="scoreboard-p1">
+          <h2>Player 1</h2>
+        </div>
+        <div className="scoreboard-p2">
+          <h2>Player 2</h2>
+        </div>
+      </div>
     </div>
   );
 }
